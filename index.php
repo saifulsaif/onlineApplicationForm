f479928e05798781a712e422f397240a87d85194
saifulsaif <saifulsaif5854@gmail.com>
2019-01-21 13:31:30 -0600

    first commit

---
+++ index.php
@@ -0,0 +1,217 @@
+<html>
+    <head>
+        <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+        <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <!------ Include the above in your HEAD tag ---------->
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
+        <link rel="stylesheet" href="style.css">
+        
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script>
+    </head>
+    <body>
+
+        <div class="row" id="contatti">
+            <div class="container mt-5" >
+
+                <div class="row" style="height:550px;">
+                    <div class="col-md-3">
+                        
+                    </div>
+                    <div class="col-md-6">
+                        <h2 class="text-uppercase mt-3 font-weight-bold text-white">Online Application Form</h2>
+                        <form class="contact-form" action="">
+                            <div class="row">
+                                <div class="col-lg-6">
+                                    <div class="form-group">
+                                        <input type="text" class="form-control mt-2" placeholder="First Name *" required>
+                                    </div>
+                                </div>
+                                <div class="col-lg-6">
+                                    <div class="form-group">
+                                        <input type="text" class="form-control mt-2" placeholder="Last Name" required>
+                                    </div>
+                                </div>
+                                <div class="col-lg-6">
+                                    <div class="form-group">
+                                        <input type="text" class="form-control mt-2" placeholder="Father's Name" required>
+                                    </div>
+                                </div>
+                                <div class="col-lg-6">
+                                    <div class="form-group">
+                                        <input type="text" class="form-control mt-2" placeholder="Mother's Name" required>
+                                    </div>
+                                </div>
+                                <div class="col-lg-6">
+                                    <div class="form-group">
+                                        <input type="text" class="form-control mt-2" placeholder="Number/WhatApp *" required>
+                                    </div>
+                                </div>
+                                <div class="col-lg-6">
+                                    <div class="form-group">
+                                        <input type="email" class="form-control mt-2" placeholder="Email *" required>
+                                    </div>
+                                </div>
+                                <div class="col-lg-12">
+                                    <div class="form-group">
+                                        <input type="text" class="form-control mt-2" placeholder="Address " required>
+                                    </div>
+                                </div>
+                                 
+                                 </div>
+                            <hr>
+                            <div class="row">
+                                 <div class="col-lg-12">
+                                    <div class="form-group">
+                                        <h6>Passing Year</h6>
+                                    </div>
+                                </div>
+                             <div class="col-lg-6">
+                                    <div class="form-group">
+                                         <h6>10th*</h6>
+                                         <select class="form-control" id="exampleFormControlSelect1">
+                                    <option>Select Year</option>
+                                    <option>2010</option>
+                                    <option>2011</option>
+                                    <option>2012</option>
+                                    <option>2013</option>
+                                    <option>2014</option>
+                                    <option>2015</option>
+                                    <option>2016</option>
+                                    <option>2017</option>
+                                    <option>2018</option>
+                                  </select>
+                                    </div>
+                                </div>
+                                
+                                <div class="col-lg-6">
+                                    <div class="form-group">
+                                         <h6>12th*</h6>
+                                         <select class="form-control" id="exampleFormControlSelect1">
+                                    <option>Select Year</option>
+                                    <option>2010</option>
+                                    <option>2011</option>
+                                    <option>2012</option>
+                                    <option>2013</option>
+                                    <option>2014</option>
+                                    <option>2015</option>
+                                    <option>2016</option>
+                                    <option>2017</option>
+                                    <option>2018</option>
+                                  </select>
+                                    </div>
+                                </div>
+                                <div class="col-lg-12">
+                                    
+                                        <h6>Obtain Mark's</h6>
+                                  
+                                </div>
+                                 <div class="col-lg-4">
+                                    <div class="form-group">
+                                        <input type="text" class="form-control mt-2" placeholder="Physics " required>
+                                    </div>
+                                </div>
+                                <div class="col-lg-4">
+                                    <div class="form-group">
+                                        <input type="text" class="form-control mt-2" placeholder="Chemistry " required>
+                                    </div>
+                                </div>
+                                <div class="col-lg-4">
+                                    <div class="form-group">
+                                        <input type="text" class="form-control mt-2" placeholder="Biology " required>
+                                    </div>
+                                </div>
+                            </div>
+                               <hr>
+                               <div class="row">
+                                   <div class="col-lg-12">
+                                    <div class="form-group">
+                                        <h6>Collage List</h6>
+                                    </div>
+                                </div>
+                               
+                                
+                               
+                               <div class="col-lg-10">
+                                    <div class="form-group">
+                                        <div class="form-group">
+                                         <select class="form-control" id="exampleFormControlSelect1">
+                                    <option>Select Collage Here</option>
+                                    <option>Diabetic Association Medical College</option>
+                                    <option>Mark's Medical College</option>
+                                    <option>2012</option>
+                                    <option>2013</option>
+                                    <option>2014</option>
+                                    <option>2015</option>
+                                    <option>2016</option>
+                                    <option>2017</option>
+                                    <option>2018</option>
+                                  </select>
+                                    </div>
+                                    </div>
+                                </div>
+                                   <div class="col-lg-1">
+                                       <a href="#" data-toggle="modal" data-target="#myModal">
+                                           <img height="40px" width="80px" src="img/icon.png">
+                                       </a>
+                                   </div>
+                                <div class="col-12">
+                                    <div class="form-group">
+                                        <textarea class="form-control" id="exampleFormControlTextarea1" placeholder="Note" rows="3" required></textarea>
+                                    </div>
+                                </div>
+                                <div class="col-lg-12">
+                                    <div class="form-group">
+                                        <h6>Attachment Files</h6>
+                                        <input type="file" class="form-control mt-2" placeholder="Address " required>
+                                    </div>
+                                </div>
+                                <div class="col-12">
+                                    <div class="btn">
+                                            <button class="btn btn-light" type="submit">SUBMIT</button>
+                                     </div>
+                                </div>
+                                   <!-- Button to Open the Modal -->
+
+
+<!-- The Modal -->
+<div class="modal" id="myModal">
+  <div class="modal-dialog">
+    <div class="modal-content">
+
+      <!-- Modal Header -->
+      <div class="modal-header">
+        <h4 class="modal-title">Modal Heading</h4>
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+      </div>
+
+      <!-- Modal body -->
+      <div class="modal-body">
+        Modal body..
+      </div>
+
+      <!-- Modal footer -->
+      <div class="modal-footer">
+        <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+      </div>
+
+    </div>
+  </div>
+</div>
+                               </div>
+                            </div>
+                        </form>
+                  
+                    </div>
+                   
+
+                </div>
+            </div>
+        </div>
+
+       
+
+    </body>
+</html>
